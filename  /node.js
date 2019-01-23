ba8c24c4a343f70255939c9ae3d16972972f01dc
Zafar Kudratov <zafarkudratov@gmail.com>
2019-01-23 21:28:19 +0200

    commit User

---  /node.js
+++  /node.js
@@ -1,9 +1,22 @@
 var Event = require('events').EventEmitter;
+var emt = new Event();
 
-emt.on('myEvent', function(a, b){
-    console.log('Arguments:', arguments.length);
-    console.log('Params', arguments.callee.length);
-    // console.log(a, b);
+
+// emt.on('myEvent', function(a, b){
+//     console.log('Arguments:', arguments.length);
+//     console.log('Params', arguments.callee.length);
+//     // console.log(a, b);
+// });
+
+emt.on('myEvent', function(data){
+    console.log(data.name.toUppcase());
+    console.log(data.code.toLowercase());
+});
+
+var user = {
+    name: 'Adam',
+    code: 'DGE34G'
 };
 
-emt.emit('myEvent', 2, 5, 6, 'hello');
+
+emt.emit('myEvent', user);
